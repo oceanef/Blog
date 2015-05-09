e07d97d42f9d640c2965ea792ae8cf77279577ea
oceanef <oceane.fievet@gmail.com>
2015-05-09 15:59:24 -0400

    [feature] Save Posts on Blog, Show them, and show the index with the date

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
 	def index
+		@posts = Post.all.order('created_at DESC')
 	end
 
 	def new
@@ -8,9 +9,13 @@ class PostsController < ApplicationController
 
 	def create
 		@post = Post.new(post_params)
-		
 		@post.save
+		
 		redirect_to @post
+	end
+
+	def show
+		@post = Post.find(params[:id])
 	end
 
 	private

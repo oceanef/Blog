aaac483c01631811cc0322be7ebd3932268807c2
oceanef <oceane.fievet@gmail.com>
2015-05-18 20:40:13 -0400

    [feature] add photo to a post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ActiveRecord::Base
 
 	validates :title, presence: true, length: {minimum: 5}
 	validates :body, presence: true
+
+	has_attached_file :image, :styles => { :medium => "300x300>" }
+	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
 end
